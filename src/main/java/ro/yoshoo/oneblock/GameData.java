c28ab07968fd37c43b15b8067cdfd0ee7a010c2c
yoshoodev <alex@yoshoo.ro>
2022-03-25 17:49:54 +0200

    Initialize the OBCommand and getters and setters for on variable.

--- src/main/java/ro/yoshoo/oneblock/GameData.java
+++ src/main/java/ro/yoshoo/oneblock/GameData.java
@@ -1,10 +1,15 @@
 package ro.yoshoo.oneblock;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.scheduler.BukkitTask;
+import ro.yoshoo.oneblock.command.OBCommand;
+import ro.yoshoo.oneblock.data.LevelData;
 import ro.yoshoo.oneblock.data.PlayerData;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class GameData {
     Oneblock plugin;
@@ -12,9 +17,18 @@ public class GameData {
         plugin = instance;
     }
 
-    boolean on = false;
+    private static boolean on = false;
 
     private List<PlayerData> players = new ArrayList<>();
+    private List<LevelData> levels = new ArrayList<>();
+
+    public static boolean isOn() {
+        return on;
+    }
+
+    public static void setOn(boolean on) {
+        GameData.on = on;
+    }
 
     public List<PlayerData> getPlayers() {
         return players;
@@ -24,7 +38,27 @@ public class GameData {
         this.players = players;
     }
 
-    void start(){
-        BukkitTask logic = new GameLogic(plugin, this).runTaskTimer(plugin, 64L,64L);
+    public List<LevelData> getLevels() {
+        return levels;
+    }
+
+    public void setLevels(List<LevelData> levels) {
+        this.levels = levels;
+    }
+
+    private void initCommands(){
+        Objects.requireNonNull(plugin.getCommand("oneblock")).setExecutor(new OBCommand(plugin, this));
+    }
+
+    public void start(){
+        long frequency = plugin.getConfig().getLong("frequency", 7L);
+        if(!on) {
+            initCommands();
+            BukkitTask logic = new GameLogic(plugin, this).runTaskTimer(plugin, frequency, frequency*2);
+            setOn(!logic.isCancelled());
+            Bukkit.getLogger().info(ChatColor.GREEN + "Game logic started successfully !");
+        } else {
+            Bukkit.getLogger().info(ChatColor.YELLOW + "Game logic is already running !");
+        }
     }
 }
