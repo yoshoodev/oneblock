0f37442bca847d5d9d732d5b7b5975b0e20ff8a8
yoshoodev <alex@yoshoo.ro>
2022-03-25 04:11:31 +0200

    Added GameData class

---
+++ src/main/java/ro/yoshoo/oneblock/GameData.java
@@ -0,0 +1,30 @@
+package ro.yoshoo.oneblock;
+
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameData {
+    Oneblock plugin;
+    public GameData(Oneblock instance){
+        plugin = instance;
+    }
+
+    boolean on = false;
+
+    private List<PlayerData> players = new ArrayList<>();
+
+    public List<PlayerData> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<PlayerData> players) {
+        this.players = players;
+    }
+
+    void start(){
+        BukkitTask logic = new GameLogic(plugin, this).runTaskTimer(plugin, 64L,64L);
+    }
+}
